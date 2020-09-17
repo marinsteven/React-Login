ba9ea5dbb18748dc3a6e64df7b66867710e48937
marinsteven <elisaxtreme2@gmail.com>
2020-09-17 16:00:48 +0900

    Add files via upload

---
+++ precache-manifest.3d22526ea047f980d22aa94d6b0f4e13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d704cb397adc944b5534e0371b988af",
+    "url": "./index.html"
+  },
+  {
+    "revision": "972e5a4ba87b945f7594",
+    "url": "./static/css/2.614afd85.chunk.css"
+  },
+  {
+    "revision": "efe80fce8d193d45693a",
+    "url": "./static/css/main.3049cb0d.chunk.css"
+  },
+  {
+    "revision": "972e5a4ba87b945f7594",
+    "url": "./static/js/2.fe1f49ce.chunk.js"
+  },
+  {
+    "revision": "efe80fce8d193d45693a",
+    "url": "./static/js/main.af39fcb6.chunk.js"
+  },
+  {
+    "revision": "bb8a3740f25fef375b9e",
+    "url": "./static/js/runtime-main.67d1021e.js"
+  }
+]);
